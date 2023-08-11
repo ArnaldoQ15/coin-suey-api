4699af551abde0d16d62183c5b76450a97e9471b
Arnaldo Fagundes <arnaldo.s.fagundes@gmail.com>
2023-08-11 20:29:43 -0300

    Remove unnecessary dependencies

--- src/main/java/com/suey/coin/CoinSueyApplication.java
+++ src/main/java/com/suey/coin/CoinSueyApplication.java
@@ -1,13 +1,14 @@
 package com.suey.coin;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class CoinSueyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoinSueyApplication.class, args);
+	public static void main(final String[] args) {
+		run(CoinSueyApplication.class, args);
 	}
 
 }
